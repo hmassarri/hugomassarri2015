4d9549350a06a9c79b4eea9750c1e005ff6f900b
hugo <hugo@hugomassarri.com>
2015-02-06 00:00:25 -0500

    First commit - working wireframe

---
+++ head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Test Page</title>
+		<meta name="description" content="Blueprint: Slide and Push Menus" />
+		<meta name="keywords" content="sliding menu, pushing menu, navigation, responsive, menu, css, jquery" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/custom.css" />
+		<link rel="stylesheet" type="text/css" href="css/menu.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<script src="lib/modernizr.custom.js"></script>
+	</head>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include 'head.php'; ?>
+
+	<body class="cbp-spmenu-push">
+
+
+		<div class="container">
+
+			<div class="row">
+
+				<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
+					<div id="menuCloseButton" class="col-md-2 pull-right closeButton"><p class="text-right">Close</p></div>
+					<h3>Menu</h3>
+					<a href="#">Link 1</a>
+					<a href="#">Link 2</a>
+					<a href="#">Link 3</a>
+					<a href="#">Link 4</a>
+					<a href="#">Link 5</a>
+					<a href="#">Link 6</a>
+				</nav>
+
+			</div>
+
+			<div id="row1" class="row">
+
+				<div id="brandlogo" class="col-md-6 pull-left"></div>
+				<div class="col-md-6 pull-right">
+					<p class="text-right">
+						<button type="button" class="menuButton" id="showRightPush">
+							<a href="#">Menu</a>
+						</button>
+					</p>
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div id="mainContent" class="col-md-12"><img src="images/placeholder1.png" /></div>
+			</div>
+
+		</div>
+		<?php include 'classie.php'; ?>
+	</body>
+</html>
