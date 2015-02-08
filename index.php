e9919f90456d59424f80ab5e4e51f49f00cc489b
hugo <hugo@hugomassarri.com>
2015-02-08 03:58:57 -0500

    addressed the close button on the menu - fixed

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
 			<div class="row">
 
 				<nav id="sidebar" class="sidebar sidebar-vertical sidebar-right">
-					<div id="menuCloseButton" class="col-md-3 pull-right closeButton">
-						<p class="text-right">Close</p>
+					<div id="menuCloseButton" class="col-md-4 pull-right closeButton">
+						<p>X</p>
 					</div>
+
 					<h3>Menu</h3>
 					<a id="index" href="index.php">index.php</a>
 					<a id="link1" href="link1.php">link1.php</a>
@@ -18,6 +19,10 @@
 					<a href="#">Link 4</a>
 					<a href="#">Link 5</a>
 					<a href="#">Link 6</a>
+					<a href="#">Link 7</a>
+					<a href="#">Link 8</a>
+					<a href="#">Link 9</a>
+					<a href="#">Link A</a>
 				</nav>
 
 			</div>
