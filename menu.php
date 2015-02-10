1f973f15c21d9e0c6f1c455e53592245c2240721
Hugo <hugo@Macintosh.local>
2015-02-10 09:31:03 -0500

    menu.php and menu.css updated with fixes

--- menu.php
+++ menu.php
@@ -1,13 +1,17 @@
 			<div class="row">
 				<nav id="sidebar" class="sidebar sidebar-vertical sidebar-right">
 					<div id="menuCloseButton" class="col-md-4 pull-right closeButton">
-						<span class="glyphicon glyphicon-remove"></span>
+						<p>Close</p>
 					</div>
 					<h3>Menu</h3>
 					<a id="index" href="index.php">index.php<i class="icon-home"></i></a>
 					<a id="link1" href="link1.php">link1.php</a>
 					<a id="link2" href="link2.php">link2.php</a>
 					<h3>Projects</h3>
+					<a href="http://iamdn.rutgers.edu/" target="_blank">IAMDN Rutgers</a>
+					<a href="http://chembuilding.rutgers.edu/" target="_blank">Rutgers Chemistry Building</a>
+					<a href="http://http://irw.rutgers.edu/" target="_blank">Insitute for Research on Women</a>
+					<a href="http://http://www.chadsaharic.com" target="_blank">Chad Saharic</a>
 					<a href="#">Chad Saharic</a>
 					<a href="#">WRAM</a>
 					<a href="#">JUMA Creative</a>
